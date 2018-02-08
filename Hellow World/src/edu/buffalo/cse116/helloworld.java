94c5e276a68e3546b28aab3876cecb32322c9367
rishengt <rishengt@ubps-vF17-lp118.itorg.ad.buffalo.edu>
2018-02-08 09:06:54 -0500

    try

---
+++ Hellow World/src/edu/buffalo/cse116/helloworld.java
@@ -0,0 +1,11 @@
+package edu.buffalo.cse116;
+
+public class helloworld {
+
+	public static void main(String[] args) {
+		
+		System.out.println("hello world");// TODO Auto-generated method stub
+
+	}
+
+}
